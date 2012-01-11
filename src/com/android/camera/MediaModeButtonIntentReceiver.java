0211df4405936922953a29a1312ba77a51a4fc2e
Pavel Chernyak <fjfalcon@fjfalcon.ru>
2012-01-11 11:15:43 +0400

    Looks like working gallery/camera/videocamera switcher
    Change-Id: I1384bb43ad415d8ddffe2f96e23bfab55bbb5c53

--- src/com/android/camera/MediaModeButtonIntentReceiver.java
+++ src/com/android/camera/MediaModeButtonIntentReceiver.java
@@ -19,7 +19,9 @@ package com.android.camera;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.app.ActivityManager;
 import android.util.Log;
+import java.util.List; 
 
 /**
  * {@code MediaModeButtonIntentReceiver} is invoked when the media mode button is
@@ -35,23 +37,84 @@ import android.util.Log;
 
 public class MediaModeButtonIntentReceiver extends BroadcastReceiver {
 
+
+	private final String cameraApp = "com.android.camera.Camera";
+	private final String videoCameraApp = "com.android.camera.VideoCamera"; 
+	private final String galleryApp =  "com.cooliris.media.Gallery"; 
+
+    private void startGallery(Context paramContext)
+	{
+		Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+		i.setType("image/*");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		paramContext.startActivity(i);
+	}
+
+    private void startCamera(Context paramContext)
+	{
+		Intent i = new Intent(Intent.ACTION_MAIN);
+		i.setClass(paramContext, Camera.class);
+		i.addCategory(Intent.CATEGORY_LAUNCHER);
+	        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	        paramContext.startActivity(i);
+	}
+
+    private void startVideoCamera(Context paramContext)
+	{
+                Intent i = new Intent(Intent.ACTION_MAIN);
+                i.setClass(paramContext, VideoCamera.class);
+                i.addCategory(Intent.CATEGORY_LAUNCHER);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                paramContext.startActivity(i);
+	}
+    private String getCurrentTopActivity(Context context) {
+        ActivityManager mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> RunningTask = mActivityManager.getRunningTasks(1);
+        ActivityManager.RunningTaskInfo ar = RunningTask.get(0);
+        return ar.topActivity.getClassName().toString();
+    }
+
+		
     @Override
     public void onReceive(Context context, Intent intent) {
-	Log.i("MediaButtonReceiver","event recieved");
+//	Log.i("MediaButtonReceiver","event recieved");
         // Try to get the camera hardware
-        CameraHolder holder = CameraHolder.instance();
-        ComboPreferences pref = new ComboPreferences(context);
-        int cameraId = CameraSettings.readPreferredCameraId(pref);
-        if (holder.tryOpen(cameraId) == null) return;
+        //CameraHolder holder = CameraHolder.instance();
+        //ComboPreferences pref = new ComboPreferences(context);
+        //int cameraId = CameraSettings.readPreferredCameraId(pref);
+        //if (holder.tryOpen(cameraId) == null) return;
 
         // We are going to launch the camera, so hold the camera for later use
-        holder.keep();
-        holder.release();
-        Intent i = new Intent(Intent.ACTION_MAIN);
-        i.setClass(context, Camera.class);
-        i.addCategory(Intent.CATEGORY_LAUNCHER);
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        context.startActivity(i);
+        //holder.keep();
+        //holder.release();
+        //Intent i = new Intent(Intent.ACTION_MAIN);
+        //i.setClass(context, Camera.class);
+        //i.addCategory(Intent.CATEGORY_LAUNCHER);
+        //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+         //       | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //context.startActivity(i);
+	//startGallery(context);
+	
+//	List activies = (ActivityManager) context.getSystemService("activity").getRunningTasks(10); // i need more study about
+//	if (activies.get(0).equals(appName)) 
+//		{
+//			// TODO: Check camera app, and set camera or videocamera.
+//		}
+//	startGallery(context);
+	String activity = getCurrentTopActivity(context);
+	Log.i("MediaModeButtonReceiver", "Current activity: " + activity);
+	if (activity.equals(cameraApp))
+		{
+			startVideoCamera(context);
+		}
+	else if (activity.equals(videoCameraApp))
+		{
+			startGallery(context);
+		}
+	else if (activity.equals(galleryApp)) {
+			startCamera(context);
+		}
+	else startGallery(context);
+
     }
 }
